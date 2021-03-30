c0436140f0cf1c64ce0ae16f84904ad3a45c4f7b
JesseB) <jesselb1123@users.noreply.github.com>
2021-03-29 23:39:11 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,161 @@
+var homeContent = `<div class="hero one">
+<!-- IMAGE IN CSS -->
+<div class="hero-text">Struggling in class?<br />We can help!</div>
+</div>
+<div class="title">What we offer!</div>
+<div class="text">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, perferendis praesentium neque veritatis
+blanditiis magnam. Dicta ex odio corrupti fugit nam fuga itaque ducimus eaque, earum velit rem quaerat iure!
+Dignissimos totam hic fuga est in adipisci expedita optio obcaecati quia doloremque vero rerum placeat, vel
+fugiat voluptatem omnis excepturi ipsum temporibus dolorum illum molestiae tempore dolore libero.
+Voluptatum, ipsum!
+<br />
+<br />
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi nihil totam cumque, voluptates exercitationem
+similique nesciunt quod qui. Natus nesciunt amet eligendi veritatis fugit error dolorem quibusdam voluptatem
+molestias minus.
+Id, at. Reiciendis sit, dolores ducimus ipsa molestiae libero quia tenetur quibusdam beatae, laudantium sint
+quis maiores vitae necessitatibus doloremque minima. Numquam officiis tempora animi quisquam, voluptatibus
+ad a cumque.
+</div>
+<div class="hero two">
+<!-- IMAGE IN CSS -->
+<div class="hero-text">Everyone deserves excellence!</div>
+</div>
+<div class="text">
+<div class="column">
+    <div class="title">Try us!</div>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus voluptas voluptates esse aspernatur
+        asperiores tempora aut quam sed culpa laborum quisquam, odit unde doloribus alias laboriosam magni
+        temporibus accusantium suscipit.
+    </p>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus voluptas voluptates esse aspernatur
+        asperiores tempora aut quam sed culpa laborum quisquam, odit unde doloribus alias laboriosam magni
+        temporibus accusantium suscipit.
+    </p>
+</div>
+<div class="column">
+    <img src="assets/home-3.jpeg" alt="book on face">
+</div>
+</div>`;
+var aboutContent = `<div class="hero three">
+<!-- IMAGE IN CSS -->
+<div class="hero-text">Our Mission</div>
+</div>
+<div class="text">
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda maxime, minus nobis temporibus deserunt
+dolor accusamus molestiae labore enim doloribus consequatur necessitatibus aliquid quibusdam officiis quam
+tempora sunt aperiam dolorem?
+Maxime sed alias rem libero repellat, temporibus exercitationem officiis, illum impedit dolore totam
+deleniti nulla eum quod ducimus amet iure sint aut! Reiciendis minima ab adipisci ullam, velit commodi sunt?
+Voluptate doloribus debitis necessitatibus expedita ipsam. Itaque blanditiis dolorum harum repudiandae
+cumque esse omnis rerum, nobis adipisci delectus architecto quos ex ea animi temporibus nesciunt dolor earum
+asperiores eaque veritatis?
+</div>
+<div class="hero four">
+<!-- IMAGE IN CSS -->
+<div class="hero-text">Our Founder</div>
+</div>
+<div class="text">
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi dolor, explicabo ex culpa rem dolorum
+repudiandae minima earum dignissimos aut fugit suscipit vero accusamus facere aperiam sunt. Quos, modi
+itaque.
+Earum beatae alias voluptates! Nobis ex unde ad consequatur libero eligendi, quo veniam voluptatum commodi!
+Voluptatem, libero est temporibus eius aliquid explicabo dolore modi autem, illo earum ad, nobis
+consequatur!
+<br />
+<br />
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque beatae rerum eum ullam dicta perferendis iste
+iure, corrupti dolore accusamus amet quibusdam perspiciatis accusantium illo necessitatibus aliquam sit et
+temporibus.
+</div>
+<div class="hero five">
+<!-- IMAGE IN CSS -->
+<div class="hero-text">Listen to our happy patrons!</div>
+</div>
+<div class="text">
+"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur optio asperiores, pariatur delectus
+odit odio ipsum similique quae unde, perspiciatis eveniet libero facere facilis. Reprehenderit nesciunt
+beatae commodi reiciendis libero."
+</div>
+<div class="quote">~Patron</div>
+<div class="text">
+"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur optio asperiores, pariatur delectus
+odit odio ipsum similique quae unde, perspiciatis eveniet libero facere facilis. Reprehenderit nesciunt
+beatae commodi reiciendis libero."
+</div>
+<div class="quote">~Patron</div>`;
+var productsContent = `<div class="hero six">
+<!-- IMAGE IN CSS -->
+<div class="hero-text">We're here to help you learn</div>
+</div>
+<div class="text">
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, debitis omnis velit, sit expedita commodi
+consequatur exercitationem accusantium eos nulla culpa illum blanditiis tenetur dignissimos molestiae
+ratione! Quod, vitae odio.
+Sit voluptatibus beatae deleniti nisi possimus alias accusamus itaque vitae nobis sapiente quaerat tempore
+soluta, ipsum iste dolores ea saepe porro consequatur nostrum enim aspernatur voluptas. Et vel
+exercitationem voluptates!
+</div>
+<div class="service">
+<!-- IMAGE IN CSS -->
+<div class="service-text">One Week of Tutoring</div>
+</div>
+<div class="service">
+<!-- IMAGE IN CSS -->
+<div class="service-text">Two Weeks of Tutoring</div>
+</div>
+<div class="service">
+<!-- IMAGE IN CSS -->
+<div class="service-text">Three Weeks of Tutoring</div>
+</div>`;
+var contactContent = `<div class="hero seven">
+<!-- IMAGE IN CSS -->
+<div class="hero-text">Want to be a Tutor?</div>
+</div>
+<div class="text">
+<button>Join Us!</button>
+</div>
+<div class="text">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, tempore quibusdam. Mollitia commodi odio
+praesentium cupiditate repellendus necessitatibus laboriosam culpa pariatur consequuntur, beatae nihil
+soluta numquam, fuga quas dicta distinctio.
+Assumenda consectetur fugit et accusamus possimus iusto praesentium repudiandae, nostrum voluptate magni
+reprehenderit, voluptates quam sint at beatae dignissimos illo! Illum incidunt consequatur cum consequuntur
+quasi et est unde deleniti!
+</div>
+<div class="text">
+<div class="column">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates labore accusamus sequi placeat omnis
+    aliquid, fuga cumque ab pariatur quidem, dolore excepturi molestias incidunt tempore debitis sunt animi
+    eius! Incidunt!
+    <br />
+    <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ipsum laborum in quis officiis rerum
+    veniam porro, doloribus animi temporibus maxime sint vel iusto perspiciatis ad doloremque repudiandae,
+    libero recusandae.
+</div>
+<div class="column">
+    <img src="assets/contact-2.png" alt="">
+</div>
+</div>`;
+
+function init() {
+    // this is where the instructions go
+    $("nav a").click(function (e) {
+        let btnID = this.id;
+        console.log("clicked " + btnID);
+        let contentID = btnID + "Content";
+
+        $("#app").html(eval(contentID));
+        // $("#app").append(btnID);
+        // $("#app").prepend(btnID);
+    });
+}
+
+// this checks for all elements to be read by the browser
+$(document).ready(function () {
+    init();
+})
